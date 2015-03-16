f297b98ac9d40ae8291eebf7fff2d8cc6723b3bd
acodelib <andrei_gurguta@yahoo.com>
2015-03-16 14:54:52 +0000

    progressed 1%

--- learing_tests.py
+++ learing_tests.py
@@ -5,23 +5,36 @@ import tkinter
 
 class GuiControl(object):
     def __init__(self,Parent:'root application from Tk'):
-        FirstMsg = tkinter.Label(Parent,fg="red",bg="black", text = "Hello World, this is a message from TKinter, delivered as a lable")
-        FirstMsg.pack()
-        SecMsg = tkinter.Label(Parent,justify = "left", text = "this is another test just to test what ever is going on there")
-        SecMsg.pack()
-        Mbox = tkinter.Message(Parent,text="this is another test but comming in from a different place")
-        Mbox.pack()
+        self.App = Parent
+        self.App.title ("ceva")
+        self.App.protocol("WM_DELETE_WINDOW",self.trapClose)
+        self.Frame1 = tkinter.Frame(self.App)
+        self.Frame1.pack()
+        self.Frame2 = tkinter.Frame(self.App)
+        self.Frame2.pack()
+        self.FirstMsg = tkinter.Label(self.Frame1,fg="red",bg="black", text = "Hello World, this is a message from TKinter, delivered as a lable")
+        self.FirstMsg.pack(side = "left")
+        self.SecMsg = tkinter.Label(self.Frame1, text = "this is another test just to test what ever is going on there")
+        self.SecMsg.pack(side = "left")
+        self.Mbox = tkinter.Message(self.Frame1,text="this is another test but comming in from a different place")
+        self.Mbox.pack(side = "left")
 
+        self.OK = tkinter.Button(self.Frame1,text = "salut")
+        self.OK["bg"] = "red"
+        self.OK["fg"] = "black"
+        self.OK.pack(side="bottom",anchor = "e")
+        self.OK.bind("<Button-1>",self.pushed)
 
-
-
-
+    def pushed(self,event):
+        print("just got pressed {}".format(event))
+    def trapClose(self):
+        print("just got closed")
+        self.App.destroy()
 
 def main():
     Application = tkinter.Tk()
-    GuiControl(Application)
+    GuiControl(Application)    
     Application.mainloop()
 
 if __name__=='__main__':
     main()
-
